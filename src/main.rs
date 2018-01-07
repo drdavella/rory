0284df0ff0a938ceaa9ba968e1ef0351e26837ca
Daniel D'Avella <drdavella@gmail.com>
2018-01-07 16:36:09 -0500

    Use clap crate for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
-extern crate getopts;
-use getopts::Options;
-use std::env;
+extern crate clap;
+use clap::{App, Arg};
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+
+    let input_arg =
+        Arg::with_name("ROM_FILE")
+             .help("Input ROM file name")
+             .required(true)
+             .index(1);
+
+    let debug_arg =
+        Arg::with_name("debug")
+            .short("d")
+            .long("debug")
+            .help("Run emulator with debug output");
+
+    App::new("rory")
+        .version("0.1.0")
+        .about("GameBoy emulator")
+        .author("Dan D'Avella")
+        .arg(input_arg)
+        .arg(debug_arg)
+        .get_matches();
 }
