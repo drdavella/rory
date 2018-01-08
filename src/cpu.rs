125c7072e01fb16a0384d15743bf74cf006a22de
Daniel D'Avella <drdavella@gmail.com>
2018-01-07 20:00:40 -0500

    Create emulator state object, pass to main loop

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,12 +1,18 @@
+struct GameState {
+    pc: u16
+}
+
 pub fn emulate(rom_array: Vec<u8>) {
 
-    let mut index = 0;
+    /* initialize emulator state */
+    let mut state = GameState{ pc: 0 };
+
     loop {
-        index = decode(&rom_array, index);
+        decode(&rom_array, &mut state);
     }
 }
 
-fn decode(rom_array: &Vec<u8>, index: usize) -> usize {
-    println!("0x{:02x}", rom_array[index]);
-    index + 1
+fn decode(rom_array: &Vec<u8>, state: &mut GameState) {
+    println!("0x{:02x}", rom_array[state.pc as usize]);
+    state.pc += 1;
 }
