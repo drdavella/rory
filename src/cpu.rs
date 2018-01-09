ff971f7628bac18eebe71ab3a8e13f54eae88788
Daniel D'Avella <drdavella@gmail.com>
2018-01-08 21:20:02 -0500

    Interpret first opcodes in event loop

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,7 +5,7 @@ struct GameState {
 pub fn emulate(rom_array: Vec<u8>) {
 
     /* initialize emulator state */
-    let mut state = GameState{ pc: 0 };
+    let mut state = GameState{ pc: 0x100 };
 
     loop {
         decode(&rom_array, &mut state);
@@ -13,6 +13,17 @@ pub fn emulate(rom_array: Vec<u8>) {
 }
 
 fn decode(rom_array: &Vec<u8>, state: &mut GameState) {
-    println!("0x{:02x}", rom_array[state.pc as usize]);
-    state.pc += 1;
+    let pc = state.pc as usize;
+    let opcode = rom_array[pc as usize];
+    let code_bytes = rom_array.get(pc..pc+3);
+
+    let msg = match opcode {
+        /* NOP */
+        0x00 => { state.pc += 1; "NOP" }
+        /* UNCONDITIONAL JUMP IMM */
+        0xc3 => { state.pc +=1; "JUMP" }
+        _ => panic!("Unrecognized opcode 0x{:02x} at pc 0x{:04x}", opcode, pc)
+    };
+
+    println!("pc[0x{:04x}]=0x{:02x} {}", pc, opcode, msg);
 }
