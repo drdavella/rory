be4a3b8c44dc92245d74f5ea5f22acefbc531f11
Daniel D'Avella <drdavella@gmail.com>
2018-01-09 20:22:09 -0500

    Incremental steps towards unconditional jump

--- src/cpu/jump.rs
+++ src/cpu/jump.rs
@@ -1,6 +1,7 @@
 use cpu::types;
 
 
-pub fn hey(whatever: &types::GameState) {
-    println!("HEY!");
+pub fn uncond_imm(state: &mut types::GameState, code_bytes: &[u8]) -> String {
+    state.pc += 1;
+    String::from(format!("0x{:02x}", code_bytes[0]))
 }
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -15,13 +15,17 @@ pub fn emulate(rom_array: Vec<u8>) {
 fn decode(rom_array: &Vec<u8>, state: &mut types::GameState) {
     let pc = state.pc as usize;
     let opcode = rom_array[pc as usize];
-    let code_bytes = rom_array.get(pc..pc+3);
+    let code_bytes = match rom_array.get(pc+1..) {
+        Some(result) => result,
+        _ => panic!("Bad code bytes at pc 0x{:04}", pc)
+    };
 
     let msg = match opcode {
         /* NOP */
-        0x00 => { state.pc += 1; "NOP" }
+        0x00 => { state.pc += 1; String::from("NOP") }
         /* UNCONDITIONAL JUMP IMM */
-        0xc3 => { state.pc +=1; jump::hey(&state); "JUMP" }
+        0xc3 => jump::uncond_imm(state, code_bytes),
+        /* UNRECOGNIZED INSTRUCTION */
         _ => panic!("Unrecognized opcode 0x{:02x} at pc 0x{:04x}", opcode, pc)
     };
 
