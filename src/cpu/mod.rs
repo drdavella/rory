48a548d4c17cad1f40df4c6710210a6995d3aabb
Daniel D'Avella <drdavella@gmail.com>
2018-01-26 21:13:54 -0500

    Implement load register from register (no HL)

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,11 +1,18 @@
 mod types;
 mod jump;
+mod memory;
 
 
 pub fn emulate(rom_array: Vec<u8>) {
 
     /* initialize emulator state */
-    let mut state = types::GameState{ pc: 0x100 };
+    let mut state = types::GameState{
+        pc: 0x100,
+        ticks: 0,
+        regs: types::Registers{
+            a: 0, b:0, c:0, d:0, e:0, h:0, l:0
+        },
+    };
 
     loop {
         decode(&rom_array, &mut state);
@@ -14,7 +21,7 @@ pub fn emulate(rom_array: Vec<u8>) {
 
 fn decode(rom_array: &Vec<u8>, state: &mut types::GameState) {
     let pc = state.pc as usize;
-    let opcode = rom_array[pc as usize];
+    let opcode = rom_array[pc];
     let code_bytes = match rom_array.get(pc+1..) {
         Some(result) => result,
         _ => panic!("Bad code bytes at pc 0x{:04}", pc)
@@ -22,9 +29,15 @@ fn decode(rom_array: &Vec<u8>, state: &mut types::GameState) {
 
     let msg = match opcode {
         /* NOP */
-        0x00 => { state.pc += 1; String::from("NOP") }
+        0x00 => { state.pc += 1; state.ticks+= 4; String::from("NOP") },
+        /* HALT (must be matched before LD REG) */
+        0x76 => panic!("HALT"),
         /* UNCONDITIONAL JUMP IMM */
         0xc3 => jump::uncond_imm(state, code_bytes),
+        /* DISABLE INTERRUPT */
+        0xf3 => { state.pc +=1; state.ticks += 4; String::from("DI") },
+        /* LD REG -> REG */
+        0x40 ... 0x7f => memory::load_reg(state, opcode),
         /* UNRECOGNIZED INSTRUCTION */
         _ => panic!("Unrecognized opcode 0x{:02x} at pc 0x{:04x}", opcode, pc)
     };
--- src/cpu/types.rs
+++ src/cpu/types.rs
@@ -1,3 +1,72 @@
+pub struct Registers {
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub h: u8,
+    pub l: u8
+}
+
 pub struct GameState {
-    pub pc: u16
+    pub pc: u16,
+    pub ticks: i32,
+    pub regs: Registers
+}
+
+pub enum Register {
+    A, B, C, D, E, H, L, HL
+}
+
+pub const REGISTER_LIST: [Register; 8] = [
+    Register::B, Register::C, Register::D, Register::E,
+    Register::H, Register::L, Register::HL, Register::A
+];
+
+pub fn reg_to_str(reg: &Register) -> &str {
+    match reg {
+        &Register::A => "A",
+        &Register::B => "B",
+        &Register::C => "C",
+        &Register::D => "D",
+        &Register::E => "E",
+        &Register::H => "H",
+        &Register::L => "L",
+        &Register::HL => "HL"
+    }
+}
+
+pub fn set_register(state: &mut GameState, reg: &Register, val: u8) {
+    match reg {
+        &Register::A => state.regs.a = val,
+        &Register::B => state.regs.b = val,
+        &Register::C => state.regs.c = val,
+        &Register::D => state.regs.d = val,
+        &Register::E => state.regs.e = val,
+        &Register::H => state.regs.h = val,
+        &Register::L => state.regs.l = val,
+        &Register::HL => panic!("Can't set HL as single register")
+    };
+}
+
+pub fn get_register(state: &GameState, reg: &Register) -> u8 {
+    match reg {
+        &Register::A => state.regs.a,
+        &Register::B => state.regs.b,
+        &Register::C => state.regs.c,
+        &Register::D => state.regs.d,
+        &Register::E => state.regs.e,
+        &Register::H => state.regs.h,
+        &Register::L => state.regs.l,
+        &Register::HL => panic!("Can't get HL as single register")
+    }
+}
+
+pub fn set_hl(state: &mut GameState, val: u16) {
+    state.regs.h = (val >> 8) as u8;
+    state.regs.l = (val & 0xff) as u8;
+}
+
+pub fn get_hl(state: GameState) -> u16 {
+    ((state.regs.h as u16) << 8) & (state.regs.l as u16)
 }
