ba2698be3cae75767bfdda7ffef360b4e6c4982b
Daniel D'Avella <drdavella@gmail.com>
2018-03-28 21:30:03 -0400

    Factor out common register ALU op code

--- src/cpu/alu.rs
+++ src/cpu/alu.rs
@@ -2,36 +2,33 @@ use cpu::types;
 use cpu::debug;
 
 
-pub fn and_reg(state: &mut types::GameState, index: u8) -> debug::Debug {
+fn do_op_reg<F>(op: F, state: &mut types::GameState, index: u8, label: &str) -> debug::Debug
+    where F: Fn(u8, u8) -> u8 {
+
     let source = &types::REGISTER_LIST[index as usize];
     match source {
         &types::Register::HL => panic!("AND from HL is not implemented"),
         _ => {
             let op0 = types::get_register(state, &types::Register::A);
             let op1 = types::get_register(state, source);
-            types::set_register(state, &types::Register::A, op0 & op1);
+            types::set_register(state, &types::Register::A, op(op0, op1));
             state.ticks += 4;
             state.pc += 1;
         }
     }
+    /* TODO: update flags, etc. */
 
-    debug_format!("AND {}", types::reg_to_str(source))
+    debug_format!("{} {}", label, types::reg_to_str(source))
+}
+
+pub fn and_reg(state: &mut types::GameState, index: u8) -> debug::Debug {
+    let and = |x, y| x & y;
+    do_op_reg(and, state, index, "AND")
 }
 
 pub fn xor_reg(state: &mut types::GameState, index: u8) -> debug::Debug {
-    let source = &types::REGISTER_LIST[index as usize];
-    match source {
-        &types::Register::HL => panic!("Can't XOR from HL"),
-        _ => {
-            let op0 = types::get_register(state, &types::Register::A);
-            let op1 = types::get_register(state, source);
-            types::set_register(state, &types::Register::A, op0 ^ op1);
-            state.ticks += 4;
-            state.pc += 1;
-        }
-    }
-
-    debug_format!("XOR {}", types::reg_to_str(source))
+    let xor = |x, y| x ^ y;
+    do_op_reg(xor, state, index, "XOR")
 }
 
 pub fn dec_reg(state: &mut types::GameState, opcode: u8) -> debug::Debug {
