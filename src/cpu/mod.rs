589b7e1e9e6d585ca3b40238af6cdd0b38525292
Daniel D'Avella <drdavella@gmail.com>
2019-06-22 11:29:46 -0400

    Make decode a method of GameState

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -16,12 +16,13 @@ pub fn emulate(rom_array: Vec<u8>) {
     let mut state = types::GameState::initialize(0x100, 0xfff3);
 
     loop {
-        decode(&rom_array, &mut state);
+        state.decode(&rom_array);
     }
 }
 
-fn decode(rom_array: &Vec<u8>, state: &mut types::GameState) {
-    let pc = state.pc as usize;
+impl types::GameState {
+fn decode(&mut self, rom_array: &Vec<u8>) {
+    let pc = self.pc as usize;
     let opcode = rom_array[pc];
     let code_bytes = match rom_array.get(pc+1..) {
         Some(result) => result,
@@ -30,53 +31,54 @@ fn decode(rom_array: &Vec<u8>, state: &mut types::GameState) {
 
     let _msg = match opcode {
         /* NOP */
-        0x00 => { state.pc += 1; state.ticks+= 4; debug_format!("NOP") },
+        0x00 => { self.pc += 1; self.ticks+= 4; debug_format!("NOP") },
         /* HALT (must be matched before LD REG) */
         0x76 => panic!("HALT"),
         /* UNCONDITIONAL JUMP IMM */
-        0xc3 => jump::jump_uncond_imm(state, code_bytes),
-        0xcd => jump::call_uncond_imm(state, code_bytes),
-        0x20 => jump::jump_cond_imm(state, code_bytes, jump::Condition::NotZero),
+        0xc3 => jump::jump_uncond_imm(self, code_bytes),
+        0xcd => jump::call_uncond_imm(self, code_bytes),
+        0x20 => jump::jump_cond_imm(self, code_bytes, jump::Condition::NotZero),
         /* DISABLE INTERRUPT */
-        0xf3 => { state.pc +=1; state.ticks += 4; debug_format!("DI") },
+        0xf3 => { self.pc +=1; self.ticks += 4; debug_format!("DI") },
         /* LD REG -> REG */
-        0x40 ... 0x7f => memory::load_reg(state, opcode),
+        0x40 ... 0x7f => memory::load_reg(self, opcode),
         /* ADD REG */
-        0x80 ... 0x87 => state.add_reg((opcode & 0x0f)),
+        0x80 ... 0x87 => self.add_reg((opcode & 0x0f)),
         /* AND REG */
-        0xa0 ... 0xa7 => state.and_reg((opcode & 0x0f)),
+        0xa0 ... 0xa7 => self.and_reg((opcode & 0x0f)),
         /* XOR REG */
-        0xa8 ... 0xaf => state.xor_reg((opcode & 0x0f) - 0x8),
+        0xa8 ... 0xaf => self.xor_reg((opcode & 0x0f) - 0x8),
         /* OR REG */
-        0xb0 ... 0xb7 => state.or_reg((opcode & 0xf)),
+        0xb0 ... 0xb7 => self.or_reg((opcode & 0xf)),
         /* DEC REG */
         0x05 | 0x15 | 0x25 | 0x35 | 0x0d | 0x1d | 0x2d | 0x3d =>
-            state.dec_reg(opcode),
+            self.dec_reg(opcode),
         /* STORE AND INC/DEC */
-        0x22 => memory::store_and_update(state, memory::Operation::Increment),
-        0x32 => memory::store_and_update(state, memory::Operation::Decrement),
+        0x22 => memory::store_and_update(self, memory::Operation::Increment),
+        0x32 => memory::store_and_update(self, memory::Operation::Decrement),
         /* LOAD AND INC/DEC */
-        0x2a => memory::load_and_update(state, memory::Operation::Increment),
-        0x3a => memory::load_and_update(state, memory::Operation::Decrement),
+        0x2a => memory::load_and_update(self, memory::Operation::Increment),
+        0x3a => memory::load_and_update(self, memory::Operation::Decrement),
         /* LD SINGLE WORD */
         0x06 | 0x16 | 0x26 | 0x36 | 0x0e | 0x1e | 0x2e | 0x3e =>
-            memory::load_word_imm(state, opcode, code_bytes),
+            memory::load_word_imm(self, opcode, code_bytes),
         /* LD DOUBLE WORD */
         0x01 | 0x11 | 0x21 | 0x31 =>
-            memory::load_dword_imm(state, opcode, code_bytes),
+            memory::load_dword_imm(self, opcode, code_bytes),
         /* LD IMM ADDR */
-        0xd0 => jump::ret_cond(state, jump::Condition::NoCarry),
-        0xea => memory::store_imm_addr(state, code_bytes),
+        0xd0 => jump::ret_cond(self, jump::Condition::NoCarry),
+        0xea => memory::store_imm_addr(self, code_bytes),
         /* LD INDIRECT ADDR */
-        0xe2 => memory::store_a_indirect_c(state),
-        0xf2 => memory::load_a_indirect_c(state),
+        0xe2 => memory::store_a_indirect_c(self),
+        0xf2 => memory::load_a_indirect_c(self),
         /* LDH (a8), A */
-        0xe0 => memory::store_a_mem(state, code_bytes),
+        0xe0 => memory::store_a_mem(self, code_bytes),
         /* LDH A, (a8) */
-        0xf0 => memory::load_a_mem(state, code_bytes),
+        0xf0 => memory::load_a_mem(self, code_bytes),
         /* UNRECOGNIZED INSTRUCTION */
         _ => panic!("Unrecognized opcode 0x{:02x} at pc 0x{:04x}", opcode, pc)
     };
 
     debug_println!("pc[0x{:04x}]=0x{:02x} {}", pc, opcode, _msg);
 }
+} /* impl GameState */
